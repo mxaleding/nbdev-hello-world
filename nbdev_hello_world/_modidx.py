d74e3d6f52972e707d21e8982db19099f4a8b093
andrealedingmx <a.leding@molytix.com>
2023-04-11 13:50:51 +0200

    Add 'say_hello'; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://mxaleding.github.io',
                 'git_url': 'https://github.com/mxaleding/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.foo': ('hello_world.html#foo', 'nbdev_hello_world/core.py'),
+                                        'nbdev_hello_world.core.say_hello': ('hello_world.html#say_hello', 'nbdev_hello_world/core.py')}}}
